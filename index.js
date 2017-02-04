d644bef271ceff9774ddced77f0a818f72165fdd
IM-MC <matthewchung022@hotmail.com>
2017-02-04 16:43:26 -0500

    Barebones node.js implementation w/ socket.io

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-
-server.listen(8080, function(){
-	console.log("Server running...");
+app.get('/',function(req,res){
+	res.sendfile(__dirname + '/main_page.html');
 });
 
 io.on('connection', function(socket){
-	console.log("Player connected.");
-	socket.on('disconnect', function(){
-		console.log("Player disconnected.");
+	console.log('User has been connected');
+	socket.broadcast.emit('A new user has been connected');
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+	socket.on('disconnect',function(){
+		console.log('User disconnected');
 	});
 });
 
+
+http.listen(8080, function(){
+	console.log('listening on *:808');
+});
