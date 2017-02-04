c8d7c12992c811a9f016d0f9c3e65a96ec003a47
IM-MC <matthewchung022@hotmail.com>
2017-02-04 17:34:51 -0500

    changes

--- index.js
+++ index.js
@@ -40,11 +40,9 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
   socket.on('chat message', function(msg){
   	io.emit('chat message', msg);
   });
-  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 
